330726523d51d6b7522e353b7c4b1cebf59e8425
Javier <ejviquin@gmail.com>
2023-05-14 08:42:52 -0500

    DEFINICION DE CONSTANTES
    -Crear documento de configuracion
    -Definir constantes globales
    	urls
    -Aniadir constantes a la hoja de estilos y al menu
    -Agregar carpetas e imagenes contenido

---
+++ mvc/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    define('URL','http://localhost/ProyectPresent/mvc/');
+
+    define('HOST','localhos');
+    define('DB','');
+    define('USER','root');
+    define('PASSWORD','');
+    define('CHARSET','utf8mb4');
+?>
--- mvc/views/header.php
+++ mvc/views/header.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="public/css/default.css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/default.css">
 </head>
 <body>
     <div id="header">
         <ul>
-            <li><a href="main">Inicio</a></li>
-            <li><a href="nuevo">Nuevo</a></li>
-            <li><a href="Contacto">Contacto</a></li>
+            <li><a href="<?php echo constant('URL');?>main">Inicio</a></li>
+            <li><a href="<?php echo constant('URL');?>nuevo">Nuevo</a></li>
+            <li><a href="<?php echo constant('URL');?>Contacto">Contacto</a></li>
         </ul>
     </div>
 </body>
